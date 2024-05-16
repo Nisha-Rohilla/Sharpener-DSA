672d532366b489978999916ff2514615761ca6cc
Nisha Rohilla <rohilla.nisha15@gmail.com>
2024-05-16 23:46:37 +0530

    pattern-3

--- Loops/Patterns/pattern-1.js
+++ Loops/Patterns/pattern-1.js
@@ -9,6 +9,7 @@
 function print_pattern() {
   for (let i = 0; i < 5; i++) {
     let str = "";
+    // inner loop
     for (let j = 5; j > i; j--) {
       str += "*";
     }
---
+++ Loops/Patterns/pattern-3.js
@@ -0,0 +1,15 @@
+// Print the following pattern using for loops
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+for (let i = 0; i < 5; i++) {
+  let str = "";
+  for (let j = 0; j < 5; j++) {
+    str += "*";
+  }
+  console.log(str);
+}
